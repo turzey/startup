fc7850e7985d37be0c4eb773d23dc337d4e4ccce
beni dibatia <42493674+turzey@users.noreply.github.com>
2020-11-05 23:47:40 +0200

    Update social-icon.js

--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -4,12 +4,12 @@ import { FaLinkedinIn, FaGithub } from "react-icons/fa"
 export default [
   {
     icon: <FaLinkedinIn />,
-    url: "https://www.linkedin.com/in/morgan-baker-developer-inverness/",
+    url: "https://www.linkedin.com/in/beni-dibatia-0ab7a7133/",
     name: "LinkedIn",
   },
   {
     icon: <FaGithub />,
-    url: "https://github.com/bagseye",
+    url: "https://github.com/turzey",
     name: "GitHub",
   },
 ]
